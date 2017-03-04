cc30845bf7912dc49815173efce87bea40cd7ff4
Diego Figueroa <diego.fv94@gmail.com>
2017-03-04 19:55:28 -0400

    add basic test suite of PW and FW factors

---
+++ test/finance-factors.spec.js
@@ -0,0 +1,59 @@
+/* global describe, context, it */
+import chai from 'chai'
+import * as lib from '../lib/finance-factors.min'
+const expect = chai.expect
+
+describe('finance-factors', () => {
+  describe('.singleFW()', () => {
+    context('with 5% interest', () => {
+      it('should return 1.05 over 1 period', () => {
+        expect(lib.singleFW(5, 1)).to.be.equal(1.05)
+      })
+      it('should return 1.1025 over 2 periods', () => {
+        expect(lib.singleFW(5, 2)).to.be.equal(1.1025)
+      })
+      it('should return 1.21550625 over 4 periods', () => {
+        expect(lib.singleFW(5, 4)).to.be.equal(1.21550625)
+      })
+      it('should return 1.340095640625 over 6 periods', () => {
+        expect(lib.singleFW(5, 6)).to.be.equal(1.340095640625)
+      })
+      it('should return 1.7958563260221292 over 12 periods', () => {
+        expect(lib.singleFW(5, 12)).to.be.equal(1.7958563260221292)
+      })
+    })
+  })
+  describe('.singlePW()', () => {
+    context('with 5% interest', () => {
+      it('should return 0.9523809523809523 from 1 period', () => {
+        expect(lib.singlePW(5, 1)).to.be.equal(0.9523809523809523)
+      })
+      it('should return 0.9070294784580498 from 2 periods', () => {
+        expect(lib.singlePW(5, 2)).to.be.equal(0.9070294784580498)
+      })
+      it('should return 0.822702474791882 from 4 periods', () => {
+        expect(lib.singlePW(5, 4)).to.be.equal(0.822702474791882)
+      })
+      it('should return 0.7462153966366276 from 6 periods', () => {
+        expect(lib.singlePW(5, 6)).to.be.equal(0.7462153966366276)
+      })
+      it('should return 0.5568374181775595 from 12 periods', () => {
+        expect(lib.singlePW(5, 12)).to.be.equal(0.5568374181775595)
+      })
+    })
+  })
+  describe('inverse relationship between PW and FW factors', () => {
+    context('.singlePW()', () => {
+      it('should return inverse of equivalent FW', () => {
+        expect(lib.singlePW(5, 1)).to.be.equal(1 / 1.05)
+        expect(lib.singlePW(5, 12)).to.be.equal(1 / 1.7958563260221292)
+      })
+    })
+    context('.singleFW()', () => {
+      it('should return inverse of equivalent PW', () => {
+        expect(lib.singleFW(5, 1)).to.be.equal(1 / 0.9523809523809523)
+        expect(lib.singleFW(5, 12)).to.be.equal(1 / 0.5568374181775595)
+      })
+    })
+  })
+})
