06e2b236522cb8c6f353308fc71063049054c404
Diego Figueroa <diego.fv94@gmail.com>
2017-07-04 23:27:17 -0700

    document function input and return types

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /**
  * Returns a Future Worth Factor assuming a single payment.
- * @param i 100-based interest rate e.g. 15% = 15
- * @param n number of periods
+ * @param {Number} i 100-based interest rate e.g. 15% = 15
+ * @param {Number} n number of periods
+ * @return {Number}
  */
 export function singleFW (i, n) {
   let futureWorthFactor = Math.pow((1 + (i / 100)), n)
@@ -9,9 +10,11 @@ export function singleFW (i, n) {
 }
 
 /**
- * Returns a Present Worth Factor assuming a single payment.
- * @param i 100-based interest rate e.g. 15% = 15
- * @param n number of periods
+ * Returns a Present Worth Factor assuming a single payment
+ * in the future.
+ * @param {Number} i 100-based interest rate e.g. 15% = 15
+ * @param {Number} n number of periods
+ * @return {Number}
  */
 export function singlePW (i, n) {
   let presentWorthFactor = 1 / singleFW(i, n)
